ee9bbd58eea9324c237878ab2044e05be1f922f8
Juan Fernando Misas Marin <jfmisasm@unal.edu.co>
2020-05-08 17:04:16 -0500

    d

--- app/Http/Controllers/add_asphaltene_stability_analysis_controller.php
+++ app/Http/Controllers/add_asphaltene_stability_analysis_controller.php
@@ -193,7 +193,7 @@ class add_asphaltene_stability_analysis_controller extends Controller
         $asphaltenes_d_diagnosis = DB::table('asphaltenes_d_diagnosis')->where('scenario_id', $scenary->id)->first();
         $asphaltenes_d_precipitated_analysis = DB::table('asphaltenes_d_precipitated_analysis')->where('scenario_id', $scenary->id)->first();
 
-        if ($asphaltenes_d_stability_analysis && $asphaltenes_d_diagnosis && $asphaltenes_d_precipitated_analysis && !$asphaltenes_d_stability_analysis->status_wr && !$asphaltenes_d_diagnosis->status_wr && $asphaltenes_d_precipitated_analysis->status_wr) {
+        if ($asphaltenes_d_stability_analysis && $asphaltenes_d_diagnosis && $asphaltenes_d_precipitated_analysis && $asphaltenes_d_stability_analysis->status_wr && $asphaltenes_d_diagnosis->status_wr && $asphaltenes_d_precipitated_analysis->status_wr) {
             $scenary->completo = 0;
             $scenary->save();
         } else {
@@ -426,7 +426,7 @@ class add_asphaltene_stability_analysis_controller extends Controller
         $asphaltenes_d_diagnosis = DB::table('asphaltenes_d_diagnosis')->where('scenario_id', $scenary->id)->first();
         $asphaltenes_d_precipitated_analysis = DB::table('asphaltenes_d_precipitated_analysis')->where('scenario_id', $scenary->id)->first();
 
-        if ($asphaltenes_d_stability_analysis && $asphaltenes_d_diagnosis && $asphaltenes_d_precipitated_analysis && !$asphaltenes_d_stability_analysis->status_wr && !$asphaltenes_d_diagnosis->status_wr && $asphaltenes_d_precipitated_analysis->status_wr) {
+        if ($asphaltenes_d_stability_analysis && $asphaltenes_d_diagnosis && $asphaltenes_d_precipitated_analysis && $asphaltenes_d_stability_analysis->status_wr && $asphaltenes_d_diagnosis->status_wr && $asphaltenes_d_precipitated_analysis->status_wr) {
             $scenary->completo = 0;
             $scenary->save();
         } else {

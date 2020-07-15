df285b9a0fefb0dc8cbbb91812760dcbc9ceb14c
cuartas15 <santi13hoy@hotmail.com>
2020-07-15 16:29:38 -0500

    Follow up to the previous commit

--- resources/views/multiparametricAnalysis/statistical/create.blade.php
+++ resources/views/multiparametricAnalysis/statistical/create.blade.php
@@ -25,7 +25,7 @@
 <div class="nav">
    <div class="tab">
       <ul class="nav nav-tabs" data-tabs="tabs" id="myTab">
-         <li class="active gfdgdfg"><a data-toggle="tab" href="#SB" id="SB_C">Statistical Database</a></li>
+         <li class="active"><a data-toggle="tab" href="#SB" id="SB_C">Statistical Database</a></li>
          <li><a data-toggle="tab" class="text-danger sectionDeactivated">Mineral Scale</a></li>
          <li><a data-toggle="tab" class="text-danger sectionDeactivated">Fine Blockage</a></li>
          <li><a data-toggle="tab" class="text-danger sectionDeactivated">Organic Scales</a></li>

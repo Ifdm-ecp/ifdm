580dfdd3cced7e46e76b60c373e644ae7941b003
Juan Misas <jfmisasm@unal.edu.co>
2020-10-21 16:43:57 -0500

    d

--- resources/views/js/template/iprs.blade.php
+++ resources/views/js/template/iprs.blade.php
@@ -853,7 +853,7 @@
       },
       yAxis: {
         title: {
-          text: 'Liquid Fracture'
+          text: 'Liquid Fraction'
         },
         plotLines: [{
           value: 0,
@@ -871,7 +871,7 @@
         borderWidth: 0
       },
       series: [{
-        name: 'Liquid Fracture',
+        name: 'Liquid Fraction',
         data: liquid_fracture
       }]
     });
@@ -2593,7 +2593,7 @@
       columns: [
 
       {title:"Pressure [psi]", data: 0,type: 'numeric', format: '0[.]0000000'},
-      {title:"Liquid Fracture[]",data: 1,type: 'numeric', format: '0[.]0000000'}
+      {title:"Liquid Fraction []",data: 1,type: 'numeric', format: '0[.]0000000'}
       ],
       startRows: 6,
       minSpareRows: 2,

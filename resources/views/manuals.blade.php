a72c7e64c93b69d4a10c3bc43b800f510ce072b9
Ifdm-ecp <ifdm.ecp@gmail.com>
2021-01-03 10:38:24 -0500

    manuals

--- resources/views/manuals.blade.php
+++ resources/views/manuals.blade.php
@@ -12,13 +12,14 @@
 	<div class="panel-body">
 		<div id="GD" class="panel-collapse collapse">
 		<ul>
-			<li><a href="#" onclick="window.open('{!! asset('manual.pdf') !!}')"><h4>User Manual</h4></a></li>
+			<li><a href="#" onclick="window.open('{!! asset('general_manuals/user_manual/user_manual.pdf') !!}')"><h4>User Manual</h4></a></li>
 			<li><a href="#" onclick="window.open('{!! asset('general_manuals/user_manual/ipr.pdf') !!}')"><h4>IPR</h4></a></li>
 			<li><a href="#" onclick="window.open('{!! asset('general_manuals/user_manual/asphaltenes.pdf') !!}')"><h4>Asphaltenes</h4></a></li>
 			<li><a href="#" onclick="window.open('{!! asset('general_manuals/user_manual/dissagregation.pdf') !!}')"><h4>Skin Dissagregation</h4></a></li>
-			<li><a href="#" onclick="window.open('{!! asset('general_manuals/user_manual/geomechanics.pdf') !!}')"><h4>Geomechanics</h4></a></li>
+			<!-- <li><a href="#" onclick="window.open('{!! asset('general_manuals/user_manual/geomechanics.pdf') !!}')"><h4>Geomechanics</h4></a></li> -->
 			<li><a href="#" onclick="window.open('{!! asset('general_manuals/user_manual/drilling.pdf') !!}')"><h4>Drilling & Completion</h4></a></li>
-			<li><a href="#" onclick="window.open('{!! asset('general_manuals/user_manual/fines.pdf') !!}')"><h4>Fines Migration</h4></a></li>  
+			<li><a href="#" onclick="window.open('{!! asset('general_manuals/user_manual/fines.pdf') !!}')"><h4>Fines Migration</h4></a></li>
+			<li><a href="#" onclick="window.open('{!! asset('general_manuals/user_manual/multiparametric.pdf') !!}')"><h4>Multiparametric</h4></a></li>
 		</ul>
 		</div>
 	</div>
@@ -30,12 +31,14 @@
 	<div class="panel-body">
 		<div id="GD2" class="panel-collapse collapse">
 		<ul>
-			<li><a href="#" onclick="window.open('{!! asset('help.pdf') !!}')"><h4>General Help</h4></a></li>
+			<li><a href="#" onclick="window.open('{!! asset('general_manuals/documentation/general_help.pdf') !!}')"><h4>General Help</h4></a></li>
 			<li><a href="#" onclick="window.open('{!! asset('general_manuals/documentation/ipr.pdf') !!}')"><h4>IPR</h4></a></li>
 			<li><a href="#" onclick="window.open('{!! asset('general_manuals/documentation/asphaltenes.pdf') !!}')"><h4>Asphaltenes</h4></a></li>
+			<li><a href="#" onclick="window.open('{!! asset('general_manuals/documentation/dissagregation.pdf') !!}')"><h4>Skin Dissagregation</h4></a></li>
 			<li><a href="#" onclick="window.open('{!! asset('general_manuals/documentation/fines.pdf') !!}')"><h4>Fines Migration</h4></a></li>
-			<li><a href="#" onclick="window.open('{!! asset('general_manuals/documentation/geomechanical.pdf') !!}')"><h4>Geomechanics</h4></a></li>
+			<!-- <li><a href="#" onclick="window.open('{!! asset('general_manuals/documentation/geomechanical.pdf') !!}')"><h4>Geomechanics</h4></a></li> -->
 			<li><a href="#" onclick="window.open('{!! asset('general_manuals/documentation/drilling.pdf') !!}')"><h4>Drilling and Completion</h4></a></li>
+			<li><a href="#" onclick="window.open('{!! asset('general_manuals/documentation/multiparametric.pdf') !!}')"><h4>Multiparametric</h4></a></li>
 		</ul>
 		</div>
 	</div>

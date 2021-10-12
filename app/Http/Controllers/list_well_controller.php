e7ae226212e9cd8f7e58181ed77298a784d5888c
Juan F. Misas <juanfer.misas@outlook.com>
2021-10-12 14:27:37 -0500

    s

--- app/Http/Controllers/list_well_controller.php
+++ app/Http/Controllers/list_well_controller.php
@@ -230,37 +230,37 @@ class list_well_controller extends Controller
 
                     plt::where('pozo_id', $well->id)->delete();
                     //Guardar tabla PLT
-                    $plt=new plt;
-                    $table = str_replace(",[null,null,null,null,null]","",$request->input("plt"));
-                    $top=array();
-                    $bottom=array();
-                    $pqo=array();
-                    $pqg=array();
-                    $pqw=array();
-                    $table = json_decode($table);
+                    if ($request->input("plt") != "[{}]") {
+                        $plt=new plt;
+                        $table = str_replace(",[null,null,null,null,null]","",$request->input("plt"));
+                        $top=array();
+                        $bottom=array();
+                        $pqo=array();
+                        $pqg=array();
+                        $pqw=array();
+                        $table = json_decode($table);
 
-                    dd($table, $request->input("plt"));
+                        foreach ($table as $value) {
+                            if(count($table)>=1){
+                                $plt = new plt;
+                                $plt->top = str_replace(",", ".", $value[0]);
+                                $top[] = (float)str_replace(",", ".", $value[0]);
 
-                    foreach ($table as $value) {
-                        if(count($table)>=1){
-                            $plt = new plt;
-                            $plt->top = str_replace(",", ".", $value[0]);
-                            $top[] = (float)str_replace(",", ".", $value[0]);
+                                $plt->bottom = str_replace(",",".",$value[1]);
+                                $bottom[] = (float)str_replace(",", ".", $value[1]);
 
-                            $plt->bottom = str_replace(",",".",$value[1]);
-                            $bottom[] = (float)str_replace(",", ".", $value[1]);
+                                $plt->pqo = str_replace(",", ".", $value[2]);
+                                $pqo[] = (float)str_replace(",", ".", $value[2]);
 
-                            $plt->pqo = str_replace(",", ".", $value[2]);
-                            $pqo[] = (float)str_replace(",", ".", $value[2]);
+                                $plt->pqg = str_replace(",",".",$value[3]);
+                                $pqg[] = (float)str_replace(",", ".", $value[3]);
 
-                            $plt->pqg = str_replace(",",".",$value[3]);
-                            $pqg[] = (float)str_replace(",", ".", $value[3]);
+                                $plt->pqw = str_replace(",",".",$value[4]);
+                                $pqw[] = (float)str_replace(",", ".", $value[4]);
 
-                            $plt->pqw = str_replace(",",".",$value[4]);
-                            $pqw[] = (float)str_replace(",", ".", $value[4]);
-
-                            $plt->pozo_id=$well->id;
-                            $plt->save();
+                                $plt->pozo_id=$well->id;
+                                $plt->save();
+                            }
                         }
                     }
 

f95e5f5c425e174b88f88998e1ac1d0542b0232a
Juan F. Misas <juanfer.misas@outlook.com>
2021-11-16 16:46:17 -0500

    Update add_fines_migration_diagnosis_controller.php

--- app/Http/Controllers/add_fines_migration_diagnosis_controller.php
+++ app/Http/Controllers/add_fines_migration_diagnosis_controller.php
@@ -1899,7 +1899,7 @@ class add_fines_migration_diagnosis_controller extends Controller
                                 $porosity_limit_constant = $this->interpolation($k_lab_qo, count($kite)-1, $kite_reverse, $porosity_limit_constantite_reverse);
                                 // dd($kite_reverse, $porosity_limit_constantite_reverse, $porosity_limit_constant, $k_lab_qo);
                                 if ($kk == 36) {
-                                    dd($kite_reverse, $porosity_limit_constantite_reverse, $porosity_limit_constant, $k_lab_qo);
+                                    dd($kite_reverse, $porosity_limit_constantite_reverse, $porosity_limit_constant, $k_lab_qo, $qlab, $qo);
                                 }
                             }
                             $porosity_change = $this->porosity_change($nr, $ndt * $tiempo[$kk], $tiempo[$kk], $ki, $phin, $u, $ucri_esc, $sigmaini, $dpre, $rhop, $co, $k1i, $k2i, $k3i, $k4i, $k5i, $k6i, $dpdli, $dpdlsi, $sigmai, $abi, $ab2i, $porosity_limit_constant);

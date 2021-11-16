bbc3146c896057010b965335232d6131eae6f8f9
Juan F. Misas <juanfer.misas@outlook.com>
2021-11-16 17:09:57 -0500

    Update add_fines_migration_diagnosis_controller.php

--- app/Http/Controllers/add_fines_migration_diagnosis_controller.php
+++ app/Http/Controllers/add_fines_migration_diagnosis_controller.php
@@ -926,7 +926,7 @@ class add_fines_migration_diagnosis_controller extends Controller
         #Hueco cementado
         } else if ($tpp =! 0) {
             if ($rw < 0.375) {
-                $fp3 = (1.036 * $tpp * $rp) / ((0.9932 * $tpp * $rp) + 0.7718);
+                $fp3 = (1.036 * $tpp * $rp) ;
                 $tcri_esc = $fp3 * 0.009057 * $tcri * (1.0 / $bw) * 2 * $rw * $hf / pow($rplug, 2);
             } else {
                 $fp6 = 1.0258 * ($tpp * $rp) / (0.9742 * $tpp * $rp + 0.8845);
@@ -1172,7 +1172,7 @@ class add_fines_migration_diagnosis_controller extends Controller
         { 
             $qlab[$i] = $this->rate_scaling($rw, $qlab[$i], $hf, $rplug, $tpp, $rp, $bw);
         }
-
+        
         dd($qlab, $rw, $hf, $rplug, $tpp, $rp, $bw);
 
         #Discretizando el medio (geometría radial)

72d4567a76ab0087d50d9e26b4cdca31cb0855c6
Juan F. Misas <juanfer.misas@outlook.com>
2022-07-25 18:58:38 -0500

    Update add_fines_migration_diagnosis_controller.php

--- app/Http/Controllers/add_fines_migration_diagnosis_controller.php
+++ app/Http/Controllers/add_fines_migration_diagnosis_controller.php
@@ -2179,7 +2179,7 @@ class add_fines_migration_diagnosis_controller extends Controller
             // Constatntite en cada caudal
         }
         // dd([ $pite, $crite, $kite, $porosity_limit_constantite, $cr, $porosity_limit_constant, 3, $simulation_results]);
-        return array($complete_simulated_results, $damage_resultsl, $constantite);
+        return array($complete_simulated_results, $damage_results, $constantite);
     }
 
     function set_array($original_array, $n)

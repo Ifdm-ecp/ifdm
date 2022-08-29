ed41fa00fc4b713135285fc63a57dcc8daf389e5
Juan F. Misas <juanfer.misas@outlook.com>
2022-08-29 13:53:03 -0500

    Update add_damage_variables_controller.php

--- app/Http/Controllers/add_damage_variables_controller.php
+++ app/Http/Controllers/add_damage_variables_controller.php
@@ -491,6 +491,7 @@ class add_damage_variables_controller extends Controller
         $pozo = DB::table('pozos')->where('nombre', $pozo_nombre)->first();
         if ($pozo == null) {
             //dd('pozo', $pozo, $pozo_nombre, $row);
+            dd('adadadd');
             $response_pozo = "error1";
         }
         $formacion = $worksheet->getCell('B'.$row)->getValue();

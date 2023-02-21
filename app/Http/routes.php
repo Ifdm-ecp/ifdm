0edd24dd3200ec56afa95e80a01efa77d26524f1
Juan F. Misas <pmisas@unal.edu.co>
2023-02-21 17:30:31 -0500

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4570,6 +4570,7 @@ Route::group(['middleware' => 'auth'], function(){
         $basin = intval($basin);
         $fields = Input::get('fields');
         $fields = urldecode($fields); //converts to array
+        dd($fields);
         $fields = array_map('intval', $fields);
         dd($fields);
         // pozos que se relacionen con los fields

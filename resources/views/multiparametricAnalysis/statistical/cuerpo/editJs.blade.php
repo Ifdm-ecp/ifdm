3542c7011a6f1fe3d4bda58a7292770c272c5b3c
Juan F. Misas <pmisas@unal.edu.co>
2023-04-15 13:09:10 -0500

    Update editJs.blade.php

--- resources/views/multiparametricAnalysis/statistical/cuerpo/editJs.blade.php
+++ resources/views/multiparametricAnalysis/statistical/cuerpo/editJs.blade.php
@@ -54,8 +54,9 @@
         if( $('#checkbox_general_MS').val() == 'on' ) {
             if ( $("#MS1_checkbox").prop('checked') == true ) {
                 Object.keys(<?php echo json_encode($formationsWithoutSpaces); ?>).forEach(key => {
-                    console.log($('#date_MS1'+key).val());
-                    if( $('#date_MS1'+key).val() === '') { $('#date_MS1'+key).val(d.getDate + '/' + (d.getMonth()+1) + "/" + d.getFullYear()); }
+                    formation = <?php echo json_encode($formationsWithoutSpaces); ?>[key];
+                    console.log($('#date_MS1'+formation).val());
+                    if( $('#date_MS1'+formation).val() === '') { $('#date_MS1'+formation).val(d.getDate + '/' + (d.getMonth()+1) + "/" + d.getFullYear()); }
                 });
             }
             

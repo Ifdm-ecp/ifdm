680d8ee19d0869aed74726e9c83f32af4932630c
Juan F. Misas <pmisas@unal.edu.co>
2023-04-17 07:51:02 -0500

    Update StatisticalController.php

--- app/Http/Controllers/MultiparametricAnalysis/StatisticalController.php
+++ app/Http/Controllers/MultiparametricAnalysis/StatisticalController.php
@@ -370,9 +370,32 @@ class StatisticalController extends Controller
             ]);
 
             if ($validator->fails()) {
-                return redirect('multiparametricAnalysis.statistical.edit')
+                
+                $input = $request->all();
+
+                /* se modifica el array del campo field_statistical con implode */
+                if ($request->field_statistical) {
+                    $input['field_statistical'] = implode(",", $request->field_statistical);
+                }
+        
+                /* se pasa la variable calculate al funcion edit */
+                Session::flash('calculate', $request->calculate);
+        
+                /* se ingresa los datos de la tabla statistical */
+                $statistical = Statistical::create($input);
+        
+                /* se guarda el parametro en la tabla subparameters_weight */
+                subparameters_weight::create(['multiparametric_id' => $statistical->id]);
+        
+                //se redirecciona a la vista edit de statistical
+                // return view('multiparametricAnalysis.statistical.edit', compact(['statistical']));
+                return redirect()->route('statistical.edit', $statistical->escenario_id);
+
+                return redirect()
+                    ->route('statistical.edit', $statistical->escenario_id)
                     ->withErrors($validator)
                     ->withInput();
+
             }else{
 
                 // Encontrar formaciones y nombres de campos de entrada

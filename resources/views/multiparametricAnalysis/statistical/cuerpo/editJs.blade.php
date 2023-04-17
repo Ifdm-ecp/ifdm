a76f6bb3705d9066cb5a8b558ca46e662528eaa1
Juan F. Misas <pmisas@unal.edu.co>
2023-04-17 16:48:52 -0500

    Update editJs.blade.php

--- resources/views/multiparametricAnalysis/statistical/cuerpo/editJs.blade.php
+++ resources/views/multiparametricAnalysis/statistical/cuerpo/editJs.blade.php
@@ -542,7 +542,7 @@
             html = html + '</ul><div class="tab-content">';
             flag = 0;
             @foreach($formationsWithoutSpaces as $j => $formationWithoutSpaces)
-                name = '<?php echo $title1 ?>' + element;
+                name = '<?php echo $title1 ?>' + $j;
                 if (flag == 0) {
                     html = html + '<div role="tabpanel" class="tab-pane fade in active" id="tab' + name + '">';
                     flag++;

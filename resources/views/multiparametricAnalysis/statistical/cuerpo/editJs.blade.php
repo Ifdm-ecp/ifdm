81059a3cfafb97e060b2e1ffdb55587c8466b797
Juan F. Misas <pmisas@unal.edu.co>
2023-04-17 14:13:58 -0500

    Update editJs.blade.php

--- resources/views/multiparametricAnalysis/statistical/cuerpo/editJs.blade.php
+++ resources/views/multiparametricAnalysis/statistical/cuerpo/editJs.blade.php
@@ -524,6 +524,7 @@
 
     function addInputGroup(title, destination_div) {
         
+        @php( $i = 0 )
         Object.keys(<?php echo json_encode($titles1); ?>).forEach(keyGeneral => {
 
             html = '<div role="tabpanel_formation"><ul class="nav nav-tabs" role="tablist">';
@@ -558,7 +559,7 @@
 
                 html = html + '</select></div></div></div>';
                 html = html + '<div class="row"><div class="col-md-4"><div class="form-group"><label for="value_' + name + '">Value</label> <label class="red">*</label><div class="input-group ';
-                @if ($errors->has('value_')) 
+                @if ($errors->has('value_'.$<?php echo json_encode($titles1); ?>[keyGeneral])) 
                     html = html + 'has-error';
                 @endif
                 html = html + '"><input type="text" id="value_' + name + '" name="value_' + name +'" class="form-control value_edit"><span class="input-group-addon" id="basic-addon2">-</span></div></div></div>';
@@ -589,6 +590,8 @@
 
             html = html + '</div></div>';
             $("#"+<?php echo json_encode($titles2); ?>[keyGeneral]).append(html);
+            console.log(<?php echo $i; ?>);
+            @php( $i++ )
         });
     }
 

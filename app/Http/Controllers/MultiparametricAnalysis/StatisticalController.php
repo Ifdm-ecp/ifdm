bcc8a45900e64ae9cdf59c27932dc732f414d7c6
Juan F. Misas <pmisas@unal.edu.co>
2023-06-08 03:05:37 -0500

    Update StatisticalController.php

--- app/Http/Controllers/MultiparametricAnalysis/StatisticalController.php
+++ app/Http/Controllers/MultiparametricAnalysis/StatisticalController.php
@@ -384,7 +384,7 @@ class StatisticalController extends Controller
                 // Session::flash('calculate', $request->calculate);
 
                 /* se ingresa los datos de la tabla statistical */
-                dd($input, $input['escenario_id']);
+                dd($input, $input['id_scenary']);
                 Statistical::where('escenario_id', $input['id_scenary'])->first()->update($input);
                 // $statistical = Statistical::where('escenario_id', $input['id_scenary'])->first();
                 // $OverwriteP10P90 = true;

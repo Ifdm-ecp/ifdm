ce43430ca69ede710bce09c43e3e3204ce9b370b
Juan F. Misas <pmisas@unal.edu.co>
2023-06-07 23:41:41 -0500

    Update StatisticalController.php

--- app/Http/Controllers/MultiparametricAnalysis/StatisticalController.php
+++ app/Http/Controllers/MultiparametricAnalysis/StatisticalController.php
@@ -76,8 +76,7 @@ class StatisticalController extends Controller
         Session::flash('calculate', $request->calculate);
 
         /* se ingresa los datos de la tabla statistical */
-        dd($input);
-        $statistical = Statistical::where('escenario_id', $input->scenario_id)->first()->update($input);
+        $statistical = Statistical::where('escenario_id', $input->escenario_id)->first()->update($input);
 
         //se redirecciona a la vista edit de statistical
         // return view('multiparametricAnalysis.statistical.edit', compact(['statistical']));

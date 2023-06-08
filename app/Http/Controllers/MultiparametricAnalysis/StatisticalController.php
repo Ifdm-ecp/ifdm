6b7a39c79b33a35d6a9dd607d489226c9f69568e
Juan F. Misas <pmisas@unal.edu.co>
2023-06-08 02:49:43 -0500

    Update StatisticalController.php

--- app/Http/Controllers/MultiparametricAnalysis/StatisticalController.php
+++ app/Http/Controllers/MultiparametricAnalysis/StatisticalController.php
@@ -393,7 +393,7 @@ class StatisticalController extends Controller
 
                 //se redirecciona a la vista edit de statistical
                 // return view('multiparametricAnalysis.statistical.edit', compact(['statistical']));
-                return view('statistical.edit', compact(['id_scenary', 'OverwriteP10P90']));
+                return view('multiparametricAnalysis.statistical.edit', compact(['id_scenary', 'OverwriteP10P90']));
                 // return redirect()->route('statistical.edit', $input['id_scenary'])->with(['id_scenary' => $input['id_scenary'], 'OverwriteP10P90' => $OverwriteP10P90]);
             }
 

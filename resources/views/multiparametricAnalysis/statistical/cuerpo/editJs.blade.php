30db4b3b4b2cd7aca68f7c2abfcd9512704b9b6b
Juan F. Misas <pmisas@unal.edu.co>
2023-06-08 00:10:06 -0500

    Update editJs.blade.php

--- resources/views/multiparametricAnalysis/statistical/cuerpo/editJs.blade.php
+++ resources/views/multiparametricAnalysis/statistical/cuerpo/editJs.blade.php
@@ -332,285 +332,281 @@
         setTimeout(function (){
             if( $("#statistical").prop('checked') == true ) {
                 if ($('#p10_MS1').val() === '' || $('#p90_MS1').val() === '') {
-                    console.log('yep'); 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'MS1'}, function(data) {
-                        console.log('yey');
-                        if ($('p10_MS1').val() === '') {
-                            console.log('1', data);
+                        if ($('#p10_MS1').val() === '') {
                             $('#p10_MS1').val(data[0]);
                         }
-                        if ($('p90_MS1').val() === '') {
-                            console.log('2', data);
+                        if ($('#p90_MS1').val() === '') {
                             $('#p90_MS1').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_MS2').val() === '' || $('#p90_MS4').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'MS2'}, function(data) {
-                        if ($('p10_MS2').val() === '') {
+                        if ($('#p10_MS2').val() === '') {
                             $('#p10_MS2').val(data[0]);
                         }
-                        if ($('p90_MS2').val() === '') {
+                        if ($('#p90_MS2').val() === '') {
                             $('#p90_MS2').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_MS3').val() === '' || $('#p90_MS2').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'MS3'}, function(data) {
-                        if ($('p10_MS3').val() === '') {
+                        if ($('#p10_MS3').val() === '') {
                             $('#p10_MS3').val(data[0]);
                         }
-                        if ($('p90_MS3').val() === '') {
+                        if ($('#p90_MS3').val() === '') {
                             $('#p90_MS3').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_MS4').val() === '' || $('#p90_MS3').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'MS4'}, function(data) {
-                        if ($('p10_MS4').val() === '') {
+                        if ($('#p10_MS4').val() === '') {
                             $('#p10_MS4').val(data[0]);
                         }
-                        if ($('p90_MS4').val() === '') {
+                        if ($('#p90_MS4').val() === '') {
                             $('#p90_MS4').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_MS5').val() === '' || $('#p90_MS5').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'MS5'}, function(data) {
-                        if ($('p10_MS5').val() === '') {
+                        if ($('#p10_MS5').val() === '') {
                             $('#p10_MS5').val(data[0]);
                         }
-                        if ($('p90_MS5').val() === '') {
+                        if ($('#p90_MS5').val() === '') {
                             $('#p90_MS5').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_FB1').val() === '' || $('#p90_FB1').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'FB1'}, function(data) {
-                        if ($('p10_FB1').val() === '') {
+                        if ($('#p10_FB1').val() === '') {
                             $('#p10_FB1').val(data[0]);
                         }
-                        if ($('p90_FB1').val() === '') {
+                        if ($('#p90_FB1').val() === '') {
                             $('#p90_FB1').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_FB2').val() === '' || $('#p90_FB2').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'FB2'}, function(data) {
-                        if ($('p10_FB2').val() === '') {
+                        if ($('#p10_FB2').val() === '') {
                             $('#p10_FB2').val(data[0]);
                         }
-                        if ($('p90_FB2').val() === '') {
+                        if ($('#p90_FB2').val() === '') {
                             $('#p90_FB2').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_FB3').val() === '' || $('#p90_FB3').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'FB3'}, function(data) {
-                        if ($('p10_FB3').val() === '') {
+                        if ($('#p10_FB3').val() === '') {
                             $('#p10_FB3').val(data[0]);
                         }
-                        if ($('p90_FB3').val() === '') {
+                        if ($('#p90_FB3').val() === '') {
                             $('#p90_FB3').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_FB4').val() === '' || $('#p90_FB4').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'FB4'}, function(data) {
-                        if ($('p10_FB4').val() === '') {
+                        if ($('#p10_FB4').val() === '') {
                             $('#p10_FB4').val(data[0]);
                         }
-                        if ($('p90_FB4').val() === '') {
+                        if ($('#p90_FB4').val() === '') {
                             $('#p90_FB4').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_FB5').val() === '' || $('#p90_FB5').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'FB5'}, function(data) {
-                        if ($('p10_FB5').val() === '') {
+                        if ($('#p10_FB5').val() === '') {
                             $('#p10_FB5').val(data[0]);
                         }
-                        if ($('p90_FB5').val() === '') {
+                        if ($('#p90_FB5').val() === '') {
                             $('#p90_FB5').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_OS1').val() === '' || $('#p90_OS1').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'OS1'}, function(data) {
-                        if ($('p10_OS1').val() === '') {
+                        if ($('#p10_OS1').val() === '') {
                             $('#p10_OS1').val(data[0]);
                         }
-                        if ($('p90_OS1').val() === '') {
+                        if ($('#p90_OS1').val() === '') {
                             $('#p90_OS1').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_OS2').val() === '' || $('#p90_OS2').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'OS2'}, function(data) {
-                        if ($('p10_OS2').val() === '') {
+                        if ($('#p10_OS2').val() === '') {
                             $('#p10_OS2').val(data[0]);
                         }
-                        if ($('p90_OS2').val() === '') {
+                        if ($('#p90_OS2').val() === '') {
                             $('#p90_OS2').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_OS3').val() === '' || $('#p90_OS3').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'OS3'}, function(data) {
-                        if ($('p10_OS3').val() === '') {
+                        if ($('#p10_OS3').val() === '') {
                             $('#p10_OS3').val(data[0]);
                         }
-                        if ($('p90_OS3').val() === '') {
+                        if ($('#p90_OS3').val() === '') {
                             $('#p90_OS3').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_OS4').val() === '' || $('#p90_OS4').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'OS4'}, function(data) {
-                        if ($('p10_OS4').val() === '') {
+                        if ($('#p10_OS4').val() === '') {
                             $('#p10_OS4').val(data[0]);
                         }
-                        if ($('p90_OS4').val() === '') {
+                        if ($('#p90_OS4').val() === '') {
                             $('#p90_OS4').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_OS5').val() === '' || $('#p90_OS5').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'OS5'}, function(data) {
-                        if ($('p10_OS5').val() === '') {
+                        if ($('#p10_OS5').val() === '') {
                             $('#p10_OS5').val(data[0]);
                         }
-                        if ($('p90_OS5').val() === '') {
+                        if ($('#p90_OS5').val() === '') {
                             $('#p90_OS5').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_RP1').val() === '' || $('#p90_RP1').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'RP1'}, function(data) {
-                        if ($('p10_RP1').val() === '') {
+                        if ($('#p10_RP1').val() === '') {
                             $('#p10_RP1').val(data[0]);
                         }
-                        if ($('p90_RP1').val() === '') {
+                        if ($('#p90_RP1').val() === '') {
                             $('#p90_RP1').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_RP2').val() === '' || $('#p90_RP2').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'RP2'}, function(data) {
-                        if ($('p10_RP2').val() === '') {
+                        if ($('#p10_RP2').val() === '') {
                             $('#p10_RP2').val(data[0]);
                         }
-                        if ($('p90_RP2').val() === '') {
+                        if ($('#p90_RP2').val() === '') {
                             $('#p90_RP2').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_RP3').val() === '' || $('#p90_RP3').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'RP3'}, function(data) {
-                        if ($('p10_RP3').val() === '') {
+                        if ($('#p10_RP3').val() === '') {
                             $('#p10_RP3').val(data[0]);
                         }
-                        if ($('p90_RP3').val() === '') {
+                        if ($('#p90_RP3').val() === '') {
                             $('#p90_RP3').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_RP4').val() === '' || $('#p90_RP4').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'RP4'}, function(data) {
-                        if ($('p10_RP4').val() === '') {
+                        if ($('#p10_RP4').val() === '') {
                             $('#p10_RP4').val(data[0]);
                         }
-                        if ($('p90_RP4').val() === '') {
+                        if ($('#p90_RP4').val() === '') {
                             $('#p90_RP4').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_RP5').val() === '' || $('#p90_RP5').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'RP5'}, function(data) {
-                        if ($('p10_RP5').val() === '') {
+                        if ($('#p10_RP5').val() === '') {
                             $('#p10_RP5').val(data[0]);
                         }
-                        if ($('p90_RP5').val() === '') {
+                        if ($('#p90_RP5').val() === '') {
                             $('#p90_RP5').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_ID1').val() === '' || $('#p90_ID1').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'ID1'}, function(data) {
-                        if ($('p10_ID1').val() === '') {
+                        if ($('#p10_ID1').val() === '') {
                             $('#p10_ID1').val(data[0]);
                         }
-                        if ($('p90_ID1').val() === '') {
+                        if ($('#p90_ID1').val() === '') {
                             $('#p90_ID1').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_ID2').val() === '' || $('#p90_ID2').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'ID2'}, function(data) {
-                        if ($('p10_ID2').val() === '') {
+                        if ($('#p10_ID2').val() === '') {
                             $('#p10_ID2').val(data[0]);
                         }
-                        if ($('p90_ID2').val() === '') {
+                        if ($('#p90_ID2').val() === '') {
                             $('#p90_ID2').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_ID3').val() === '' || $('#p90_ID3').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'ID3'}, function(data) {
-                        if ($('p10_ID3').val() === '') {
+                        if ($('#p10_ID3').val() === '') {
                             $('#p10_ID3').val(data[0]);
                         }
-                        if ($('p90_ID3').val() === '') {
+                        if ($('#p90_ID3').val() === '') {
                             $('#p90_ID3').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_ID4').val() === '' || $('#p90_ID4').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'ID4'}, function(data) {
-                        if ($('p10_ID4').val() === '') {
+                        if ($('#p10_ID4').val() === '') {
                             $('#p10_ID4').val(data[0]);
                         }
-                        if ($('p90_ID4').val() === '') {
+                        if ($('#p90_ID4').val() === '') {
                             $('#p90_ID4').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_GD1').val() === '' || $('#p90_GD1').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'GD1'}, function(data) {
-                        if ($('p10_GD1').val() === '') {
+                        if ($('#p10_GD1').val() === '') {
                             $('#p10_GD1').val(data[0]);
                         }
-                        if ($('p90_GD1').val() === '') {
+                        if ($('#p90_GD1').val() === '') {
                             $('#p90_GD1').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_GD2').val() === '' || $('#p90_GD2').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'GD2'}, function(data) {
-                        if ($('p10_GD2').val() === '') {
+                        if ($('#p10_GD2').val() === '') {
                             $('#p10_GD2').val(data[0]);
                         }
-                        if ($('p90_GD2').val() === '') {
+                        if ($('#p90_GD2').val() === '') {
                             $('#p90_GD2').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_GD3').val() === '' || $('#p90_GD3').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'GD3'}, function(data) {
-                        if ($('p10_GD3').val() === '') {
+                        if ($('#p10_GD3').val() === '') {
                             $('#p10_GD3').val(data[0]);
                         }
-                        if ($('p90_GD3').val() === '') {
+                        if ($('#p90_GD3').val() === '') {
                             $('#p90_GD3').val(data[1]);
                         }
                     }); 
                 }
                 if ($('#p10_GD4').val() === '' || $('#p90_GD4').val() === '') { 
                     $.get("{!! url('p10p90Colombia') !!}", {subparameterId: 'GD4'}, function(data) {
-                        if ($('p10_GD4').val() === '') {
+                        if ($('#p10_GD4').val() === '') {
                             $('#p10_GD4').val(data[0]);
                         }
-                        if ($('p90_GD4').val() === '') {
+                        if ($('#p90_GD4').val() === '') {
                             $('#p90_GD4').val(data[1]);
                         }
                     }); 

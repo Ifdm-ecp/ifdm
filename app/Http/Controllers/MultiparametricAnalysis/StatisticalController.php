b9da8ac1be0191bbaaed718a01f29a1afbd35c7a
Juan F. Misas <pmisas@unal.edu.co>
2023-06-08 03:48:09 -0500

    Update StatisticalController.php

--- app/Http/Controllers/MultiparametricAnalysis/StatisticalController.php
+++ app/Http/Controllers/MultiparametricAnalysis/StatisticalController.php
@@ -353,9 +353,9 @@ class StatisticalController extends Controller
         $duplicateFrom = isset($_SESSION['scenary_id_dup']) ? $_SESSION['scenary_id_dup'] : null;
 
         if ($calculate == true) {
-            $OverwriteP10P90 = 2;
+            $OverwriteP10P90 = "true";
         } else {
-            $OverwriteP10P90 = 1;
+            $OverwriteP10P90 = "false";
         }
         
         

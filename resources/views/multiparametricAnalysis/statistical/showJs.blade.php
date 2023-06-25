9f3c160dcd7ed7b906fc688f04fd8cdc680ff3cf
Juan F. Misas <pmisas@unal.edu.co>
2023-06-24 23:54:35 -0500

    Update showJs.blade.php

--- resources/views/multiparametricAnalysis/statistical/showJs.blade.php
+++ resources/views/multiparametricAnalysis/statistical/showJs.blade.php
@@ -19,6 +19,7 @@
         titles.shift();
         console.log(titles);
         console.log(<?php echo json_encode($datos); ?>);
+        console.log(<?php echo json_encode($tableData); ?>);
         // titles = ['Mineral Scales', 'Fine Blockage', 'Organic Scales', 'Relative Permeability', 'Induced Damage', 'Geomechanical Damage'];
         for (let i = 0; i < <?php echo json_encode($generalCheckboxes); ?>.length; i++) {
             if (<?php echo json_encode($generalCheckboxes); ?>[i] === 1) {

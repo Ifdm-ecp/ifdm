d68b01dad78e92421b38c9d5ba7f491b710fde77
Juan F. Misas <pmisas@unal.edu.co>
2023-06-27 13:59:52 -0500

    Update showJs.blade.php

--- resources/views/multiparametricAnalysis/statistical/showJs.blade.php
+++ resources/views/multiparametricAnalysis/statistical/showJs.blade.php
@@ -31,6 +31,7 @@
         }
 
         console.log('datos', <?php echo json_encode($datos); ?>, 'generalcheckboxes',  <?php echo json_encode($generalCheckboxes); ?>, 'table header', <?php echo json_encode($tableHeader); ?>, 'tabledata',  <?php echo json_encode($tableData); ?>);
+        console.log('data', data, 'categories', categories);
 
         $('#container').highcharts({
 
